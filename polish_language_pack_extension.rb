cf6f1d389272a2b5338c366ffe7e519c467a0603
Kamil Ciemniewski <ciemniewski.kamil@gmail.com>
2010-04-13 15:00:25 +0200

    update

---
+++ polish_language_pack_extension.rb
@@ -0,0 +1,9 @@
+class PolishLanguagePackExtension < Radiant::Extension
+  version "1.0"
+  description "Wsparcie dla języka polskiego w Radiant CMS"
+  url "http://digitalinventor.pl"
+
+  def activate
+		
+  end
+end
